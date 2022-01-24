2dd4fd693a78ef591f49de56322d0beb97107e45
bryannajones02 <97709674+bryannajones02@users.noreply.github.com>
2022-01-23 22:09:00 -0500

    Add files via upload

---
+++ DecoderRing.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace secondproject
+{
+    public static class DecoderRing
+    {
+
+        private static int num2 = 0;
+        private static char value =' ';
+        private static string newsentence;
+
+
+        public static string Encode(string aString)
+        {
+            char[] charArray = aString.ToCharArray();
+            int num;
+            for (num = 0; num < charArray.Length; num++) //iterate through whole array
+            {
+                num2 = charArray[num] - 1; //if encoded subtract the int value of that char by one
+                value = (char)num2;
+                charArray[num] = value;
+                newsentence = new string(charArray);
+            }
+            return newsentence;
+        }
+
+
+        
+        public static string Decode(string aString)
+        {
+            char[] charArray = aString.ToCharArray();
+            char value;
+            for (int num = 0; num < charArray.Length; num++) //iterate through whole array
+            {
+                int num2 = charArray[num] + 1; //if encoded add the int value of that char by one
+                value = (char)num2;
+                charArray[num] = value;
+                newsentence = new string(charArray);
+            }
+            return newsentence;
+           
+        }
+        
+        public static string Secret(string aString)
+        {
+            char[] charArray = aString.ToCharArray();
+            if (charArray[0] != 'x') //decode
+            {
+                return DecoderRing.Decode(aString);
+            }
+            else //encode
+            {
+                return DecoderRing.Encode(aString);
+            }
+          
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿/* Bryanna Jones, November 29, Problem 1 */
+
+using System;
+
+namespace secondproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string words;
+            char entered = ' ';
+            
+
+           do
+            {
+                Console.WriteLine("Please enter e(encode), d(decode), s(secret), " +
+               "or q(quit).");
+                entered = Console.ReadKey().KeyChar;
+                if (entered == 'q' || entered == 'Q')
+                {
+                    Console.WriteLine("Game Over !!");
+                    break;
+                }
+
+                if (entered == 'e' || entered == 'E')
+                {
+                    Console.WriteLine("Please enter a sentence");
+                    words = Console.ReadLine();
+                    char[] phrase = new char[words.Length];
+                    Console.WriteLine($"Your endoded message is {DecoderRing.Encode(words)}.");
+                }
+
+                else if (entered == 'd' || entered == 'D')
+                {
+                    Console.WriteLine("Please enter a sentence");
+                    words = Console.ReadLine();
+                    char[] phrase = new char[words.Length];
+                    Console.WriteLine($"Your decoded message is {DecoderRing.Decode(words)}.");
+                }
+                else if (entered == 's' || entered == 'S')
+                {
+                    Console.WriteLine("Please enter a sentence");
+                    words = Console.ReadLine();
+                    char[] phrase = new char[words.Length];
+                    Console.WriteLine($"Your secret message is {DecoderRing.Secret(words)}.");
+                }
+                else
+                {
+                    Console.WriteLine("Please enter e(encode), d(decode), s(secret), " +
+                "or q(quit)");
+                    entered = Console.ReadKey().KeyChar;
+                }
+            } while (entered != ' ') ;
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
